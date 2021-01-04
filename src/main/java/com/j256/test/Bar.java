3f52c595b77f9a972648c4437602bbfd786f5666
Gray Watson <gray.github@mailnull.com>
2021-01-04 15:30:27 -0500

    cp

--- src/main/java/com/j256/test/Bar.java
+++
@@ -1,7 +0,0 @@
-package com.j256.test;
-
-public class Bar {
-	// if this is commented out or made an instance field, the problem goes away
-	@SuppressWarnings("unused")
-	private static org.apache.commons.logging.Log log = org.apache.commons.logging.LogFactory.getLog(Bar.class);
-}
